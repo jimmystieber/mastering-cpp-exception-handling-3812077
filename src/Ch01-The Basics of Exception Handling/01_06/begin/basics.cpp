ab7ae95ad48c3a902234eb41b1a65f5825386f42
jimmystieber <stieber.james@gmail.com>
2024-08-25 20:36:48 +0000

    Added exception handling for bad file stream

--- src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
+++ src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
@@ -8,6 +8,10 @@ const string readConfigFile(const string& filename)
 {
     ifstream file(filename);
 
+    if (!file.good()) {
+        throw std::runtime_error("File stream is bad!");
+    }
+
     string line;
     string text;
     while (getline(file, line)) 
@@ -22,8 +26,13 @@ const string readConfigFile(const string& filename)
 int main() 
 {    
     cout << "Reading configuration:" << endl;
-    string text = readConfigFile("configuration.txt");
-    cout << text;
+    try {
+        string text = readConfigFile("configuration.txt");
+        cout << text;
+    } catch (const std::runtime_error &e) {
+        cout << e.what() << std::endl;
+        return 1;
+    }
     
     return 0;
 }
